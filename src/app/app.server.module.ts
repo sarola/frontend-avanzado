bb16b595c4b5dfd07fdec54456d816902a3390f8
sarola <48288341+sarola@users.noreply.github.com>
2019-06-16 17:21:50 +0200

    añadimos interceptor

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,18 +1,28 @@
 import { NgModule } from '@angular/core';
-import {ServerModule, ServerTransferStateModule} from '@angular/platform-server';
+import { ServerModule } from '@angular/platform-server';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
 import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
+
+// httpinterceptor para SSR
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {UniversalInterceptor} from './universal-interceptor';
+
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ServerTransferStateModule,
-    ModuleMapLoaderModule
+    ModuleMapLoaderModule,
+    FlexLayoutServerModule,
   ],
   bootstrap: [AppComponent],
+  providers: [{
+    provide: HTTP_INTERCEPTORS,
+    useClass: UniversalInterceptor,
+    multi: true
+  }],
 })
-
 export class AppServerModule {}
